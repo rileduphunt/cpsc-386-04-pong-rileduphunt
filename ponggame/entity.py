f2bd5ed3fea75d73f69703c8bb9dd5273fb44f98
Riley Hunt <hunt4riley@csu.fullerton.edu>
2022-07-31 14:58:56 -0700

    Add entity.py

---
+++ ponggame/entity.py
@@ -0,0 +1,17 @@
+# Riley Hunt
+# CPSC 386-01
+# 2022-07-31
+# hunt4riley@csu.fullerton.edu
+# @rileduphunt
+#
+# Lab 04-00
+#
+# Entity class for an entity component system.
+#
+
+
+"""TODO:test"""
+
+
+class Entity:
+    pass
